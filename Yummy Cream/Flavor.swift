662950cf13a330357e4434a5ecbf61257e46382b
NateLee <intoqasa@gmail.com>
2018-09-16 20:00:37 +0800

    UIColor+RGBA extensions(UIColor.RGB(203, 140, 58))

--- Yummy Cream/Flavor.swift
+++ Yummy Cream/Flavor.swift
@@ -6,4 +6,21 @@
 //  Copyright © 2018 M78. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+struct Flavor {
+    // MARK: - Static Properties
+    static var chocolate: Flavor {
+        return Flavor(name: "Chocolate", topColor: UIColor.RGB(203, 140, 58), bottomColor: UIColor.RGB(107, 46, 11))
+    }
+    static var vanilla: Flavor {
+        return Flavor(name: "Vanilla", topColor: UIColor.RGB(251, 248, 236), bottomColor: UIColor.RGB(230, 215, 171))
+    }
+    
+    // MARK: - Instance Properties
+    let name: String
+    let topColor: UIColor
+    let bottomColor: UIColor
+ 
+    
+}
---
+++ Yummy Cream/UIColor+RGBA.swift
@@ -0,0 +1,45 @@
+//
+//  UIColor+RGBA.swift
+//  Yummy Cream
+//
+//  Created by M78 on 9/16/18.
+//  Copyright © 2018 M78. All rights reserved.
+//
+
+import UIKit
+
+
+extension UIColor {
+    
+    static func RGB(_ red: CGFloat, _ green: CGFloat, _ blue: CGFloat) -> UIColor {
+        return RGBA(red, green, blue, 255)
+    }
+    
+    static func RGBA(_ red: CGFloat, _ green: CGFloat, _ blue: CGFloat, _ alpha: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: alpha/255)
+    }
+    
+    convenience init?(rgbaString: String) {
+        var components = rgbaString.components(separatedBy: ",")
+        
+        if components.count == 3 {
+            components.append("1.0")
+        } else if components.count != 4 {
+            return nil
+        }
+        
+        guard let redComponent = Float(components[0]),
+            let greenComponent = Float(components[1]),
+            let blueComponent = Float(components[2]),
+            let alphaComponent = Float(components[3]) else {
+                return nil
+        }
+        
+        let red = CGFloat(redComponent / 255.0)
+        let green = CGFloat(greenComponent / 255.0)
+        let blue = CGFloat(blueComponent / 255.0)
+        let alpha = CGFloat(alphaComponent / 255.0)
+        
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+}
